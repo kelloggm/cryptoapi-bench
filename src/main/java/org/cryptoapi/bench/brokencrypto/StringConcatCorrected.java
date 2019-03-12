69b31912dac8e13729e0c35e1a78b67dac5e0529
Martin Kellogg <kelloggm@cs.washington.edu>
2019-03-12 16:02:16 -0700

    string concatenation true and false positives

---
+++ src/main/java/org/cryptoapi/bench/brokencrypto/StringConcatCorrected.java
@@ -0,0 +1,28 @@
+package org.cryptoapi.bench.brokencrypto;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+
+public class StringConcatCorrected {
+
+    private final String first = "AES";
+    private final String second = "CBC";
+    private final String third = "PKCS5Padding";
+
+    public void go() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        KeyGenerator keyGen = KeyGenerator.getInstance("A" + "ES");
+        SecretKey key = keyGen.generateKey();
+        Cipher cipher = Cipher.getInstance(first + "/" + second + "/" + third);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+    }
+
+    public static void main (String [] args) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        StringConcatCorrected bc = new StringConcatCorrected();
+        bc.go();
+    }
+}
