e77dc23956ecdd159de894e649f58a38eec57b61
Martin Kellogg <kelloggm@cs.washington.edu>
2019-03-15 09:45:23 -0700

    add true negatives for ecb

---
+++ src/main/java/org/cryptoapi/bench/ecbcrypto/EcbInSymmCryptoABICorrected2.java
@@ -0,0 +1,35 @@
+package org.cryptoapi.bench.ecbcrypto;
+
+import org.checkerframework.common.value.qual.StringVal;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class EcbInSymmCryptoABICorrected2 {
+    public static final String DEFAULT_CRYPTO = "AES/CBC/PKCS5Padding";
+    private static char @StringVal("AES/CBC/PKCS5Padding") [] CRYPTO;
+    private static char @StringVal("AES/CBC/PKCS5Padding") [] crypto;
+    public void go() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        SecretKey key = keyGen.generateKey();
+        Cipher cipher = Cipher.getInstance(String.valueOf(crypto));
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+    }
+    private static void go2(){
+        CRYPTO = DEFAULT_CRYPTO.toCharArray();
+    }
+    private static void go3(){
+        crypto = CRYPTO;
+    }
+
+    public static void main (String [] args) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+        EcbInSymmCryptoABICorrected2 bc = new EcbInSymmCryptoABICorrected2();
+        go2();
+        go3();
+        bc.go();
+    }
+}
---
+++ src/main/java/org/cryptoapi/bench/ecbcrypto/EcbInSymmCryptoABSCorrected.java
@@ -0,0 +1,39 @@
+package org.cryptoapi.bench.ecbcrypto;
+
+import org.checkerframework.common.value.qual.StringVal;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class EcbInSymmCryptoABSCorrected {
+    CryptoECB0 crypto;
+    public EcbInSymmCryptoABSCorrected() {
+        String cryptoAlgo = "AES/CBC/PKCS5Padding";
+        crypto = new CryptoECB0(cryptoAlgo);
+    }
+}
+
+class CryptoECB0 {
+    @StringVal("AES/CBC/PKCS5Padding") String defAlgo;
+
+    public CryptoECB0(@StringVal("AES/CBC/PKCS5Padding") String algo) {
+        defAlgo = algo;
+    }
+
+    public void encrypt(@StringVal("AES/CBC/PKCS5Padding") String passedAlgo) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+
+        if(passedAlgo.isEmpty()){
+            passedAlgo = defAlgo;
+        }
+
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        SecretKey key = keyGen.generateKey();
+        Cipher cipher = Cipher.getInstance(passedAlgo);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+    }
+}
